c8e1aa7abcc850ca98a9eec0ebb4428d84f47007
gitaafu <aafreenwork521@gmail.com>
2024-03-07 19:02:35 +0530

    final commit by aafu

--- SHELBY_SELLER/src/app/app.module.ts
+++ SHELBY_SELLER/src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-import { AboutComponent } from './about/about.component';
+
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
@@ -15,8 +17,9 @@ import { LoginComponent } from './login/login.component';
     NavBarComponent,
     FooterComponent,
     BodyComponent,
-    AboutComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    
   ],
   imports: [
     BrowserModule,
